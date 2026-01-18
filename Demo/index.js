f2acef49120567f15c936978d2247afff56997b9
coders <coders081@gmail.com>
2026-01-18 14:46:22 +0200

    Add initial CodersAPI demo application
    Creates a simple CodersAPI app with a root endpoint that returns a welcome message and starts the server on port 3000.

---
+++ Demo/index.js
@@ -0,0 +1,11 @@
+const CodersAPI = require('coders.api');
+
+const app = new CodersAPI({
+    name: 'My New API',
+});
+
+app.get('/', (req, res) => {
+    res.ok({ message: 'Welcome to CodersAPI!' });
+});
+
+app.start(3000);
